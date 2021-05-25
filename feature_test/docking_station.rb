70d0c0df9c7556fec908de81c7bd8a07402cb8c9
Ashley Slaney <ashleyslaney@MA372.broadband>
2021-05-25 16:46:13 +0100

    error explanation

--- feature_test/docking_station.rb
+++ feature_test/docking_station.rb
@@ -1,5 +1,13 @@
-class DockingStation
-  def release_bike
+# Error type: NameError (uninitialized constant DockingStation)
+# File path: from /Users/ashleyslaney/.rvm/rubies/ruby-2.7.2/bin/irb:23:in `<main>'
+# Error line number: 23
+# The error is because the class name (DockingStation) isn't defined
+# To fix this error, we need to create a class called 'DockingStation' so we can create an instance of the class
 
-  end
-end
+
+# class DockingStation
+# 	def release_bike
+
+# 	end
+# end
+
