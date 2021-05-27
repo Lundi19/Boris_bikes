3368d9d260aaec1378525125f39df2f40d20d4a6
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-05-27 17:33:24 +0200

    14 - initialize method and empty array

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,8 @@
 require './lib/docking_station'
 
 describe DockingStation do
+  
   it {is_expected.respond_to?(:release_bike)}
-
   describe '#release_bike' do 
     it 'releases a bike' do 
       
@@ -13,22 +13,30 @@ describe DockingStation do
   end
 end
 
+  describe '#dock' do
+  it 'raises an error when full' do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+  end
+end
   it 'doesnt have enough bikes' do 
    
-   expect {subject.release_bike }.to raise_error('No bikes available!')
+   expect {subject.release_bike }.to raise_error 'No bikes available!'
   end
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
   
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
 
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
+  
+
 end 
 
 
